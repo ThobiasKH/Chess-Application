8361235eb053693d34427bfa1d496b1889f47216
ThobiasKH <pr.stfu@gmail.com>
2024-06-14 23:44:20 +0200

    Added a comment

--- src/App.java
+++ src/App.java
@@ -21,6 +21,8 @@ import moves.Move;
 import pieces.Piece;
 import player.Player;
 
+// Sick change
+
 public class App {
     private static final int DESIRED_FPS = 240;
     private static final BoardRepresentation chessBoardRep = new BoardRepresentation();
